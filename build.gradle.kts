b55136bc6a5b229e7d1795ac8fa5f3daf2154dae
apple <nikitosiksymonenko@gmail.com>
2025-06-11 22:53:57 +0200

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,37 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "1.9.23"
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("osinttool")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(23)
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("io.ktor:ktor-client-cio:2.3.7")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,46 @@
-package org.example
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
+import kotlinx.coroutines.runBlocking
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main(args: Array<String>) {
+    val parser = ArgParser("osint-search")
+    val phone by parser.option(ArgType.String, shortName = "p", description = "Phone number to search")
+    val address by parser.option(ArgType.String, shortName = "a", description = "Address to search")
+    parser.parse(args)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    runBlocking {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        if (phone != null) {
+            println("Searching for phone number: $phone")
+        }
+
+        if (address != null) {
+            println("Searching for address: $address")
+            val url = "https://nominatim.openstreetmap.org/search"
+            address?.let { safeAddress ->
+                val response: String = client.get(url) {
+                    url {
+                        parameters.append("q", safeAddress)
+                        parameters.append("format", "json")
+                    }
+                }.body()
+                println("Response: $response")
+            }
+        }
+
+        client.close()
     }
 }
